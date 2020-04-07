ff6eef4e0d528eb171fd2ec4e45831e62e527b5a
Ágocsi-Kiss Bence <akbence007@gmail.com>
2020-04-07 15:15:03 +0200

    init di Demo

---
+++ src/main/java/hu/agocsi/demo/Lecture3Application.java
@@ -0,0 +1,13 @@
+package hu.agocsi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lecture3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lecture3Application.class, args);
+    }
+
+}
