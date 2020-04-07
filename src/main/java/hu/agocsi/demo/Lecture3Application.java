cef4520ae518b970ce766cc75ab5e15bb35b5a71
Ágocsi-Kiss Bence <akbence007@gmail.com>
2020-04-07 16:21:03 +0200

    helloworld with only Spring framework

--- src/main/java/hu/agocsi/demo/Lecture3Application.java
+++ src/main/java/hu/agocsi/demo/Lecture3Application.java
@@ -1,13 +1,19 @@
 package hu.agocsi.demo;
 
+import hu.agocsi.demo.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Lecture3Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Lecture3Application.class, args);
-    }
 
+        ApplicationContext ctx = SpringApplication.run(Lecture3Application.class, args);
+
+        MyController controller = (MyController) ctx.getBean("myController");
+
+        controller.hello();
+    }
 }
