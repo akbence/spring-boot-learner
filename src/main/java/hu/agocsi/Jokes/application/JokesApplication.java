f2f2878d2b706a3db167cb7c980dde3359dd4682
Ágocsi-Kiss Bence <akbence007@gmail.com>
2020-04-08 15:49:20 +0200

    initializer of lecture3 assignment

---
+++ src/main/java/hu/agocsi/Jokes/application/JokesApplication.java
@@ -0,0 +1,13 @@
+package hu.agocsi.Jokes.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/agocsi/Jokes/application/JokesApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.agocsi.Jokes.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
