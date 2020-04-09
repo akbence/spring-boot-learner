f75e09842aed796806204d76034b01315fd2b5d7
Ágocsi-Kiss Bence <akbence007@gmail.com>
2020-04-09 11:24:50 +0200

    Xml config example

--- src/main/java/hu/agocsi/Jokes/application/JokesApplication.java
+++ src/main/java/hu/agocsi/Jokes/application/JokesApplication.java
@@ -2,8 +2,10 @@ package hu.agocsi.Jokes.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class JokesApplication {
 
 	public static void main(String[] args) {
