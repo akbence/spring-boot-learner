ebfadf1b52eaa9dca04b7db922fbc075dbc66dc7
Ágocsi-Kiss Bence <akbence007@gmail.com>
2020-04-20 11:17:19 +0200

    lecture6 : external property source. Notes: - had to remove the previous greeting controllers - had to put in manually to the target folder the prop file (lack of maven plugin)

--- src/main/java/hu/agocsi/demo/Lecture3Application.java
+++ src/main/java/hu/agocsi/demo/Lecture3Application.java
@@ -1,9 +1,6 @@
 package hu.agocsi.demo;
 
-import hu.agocsi.demo.controllers.ConstructorInjectedController;
-import hu.agocsi.demo.controllers.GetterInjectedController;
-import hu.agocsi.demo.controllers.MyController;
-import hu.agocsi.demo.controllers.PropertyInjectedController;
+import hu.agocsi.demo.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,11 +11,8 @@ public class Lecture3Application {
     public static void main(String[] args) {
 
         ApplicationContext ctx = SpringApplication.run(Lecture3Application.class, args);
-        MyController controller = (MyController) ctx.getBean("myController");
 
-        System.out.println(controller.hello());
-        System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(GetterInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+        FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+        System.out.println(fakeDataSource.getUser());
     }
 }
---
+++ src/main/java/hu/agocsi/demo/config/PropertyConfig.java
@@ -0,0 +1,37 @@
+package hu.agocsi.demo.config;
+
+import hu.agocsi.demo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+
+    @Value("${guru.username}")
+    String user;
+
+    @Value("${guru.password}")
+    String password;
+
+    @Value("${guru.dburl}")
+    String url;
+
+    @Bean
+    public FakeDataSource fakeDataSource(){
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+        fakeDataSource.setUser(user);
+        return fakeDataSource;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties(){
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+}
