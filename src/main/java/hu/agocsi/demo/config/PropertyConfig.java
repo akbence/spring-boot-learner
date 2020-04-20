2dfb32a932d3721f5c654b3cd1c5150d9b4f0f1a
Ágocsi-Kiss Bence <akbence007@gmail.com>
2020-04-20 11:24:26 +0200

    lecture6: get system environment value ("USERNAME")

--- src/main/java/hu/agocsi/demo/config/PropertyConfig.java
+++ src/main/java/hu/agocsi/demo/config/PropertyConfig.java
@@ -1,15 +1,21 @@
 package hu.agocsi.demo.config;
 
 import hu.agocsi.demo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+
+    @Autowired
+    Environment env;
 
     @Value("${guru.username}")
     String user;
@@ -25,13 +31,12 @@ public class PropertyConfig {
         FakeDataSource fakeDataSource = new FakeDataSource();
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
-        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("USERNAME"));
         return fakeDataSource;
     }
 
     @Bean
     public static PropertySourcesPlaceholderConfigurer properties(){
-        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-        return propertySourcesPlaceholderConfigurer;
+        return new PropertySourcesPlaceholderConfigurer();
     }
 }
