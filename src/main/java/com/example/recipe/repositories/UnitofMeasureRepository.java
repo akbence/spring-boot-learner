cc49cf4fb27f6f108791665279ff7d00775bddce
Ágocsi-Kiss Bence <akbence007@gmail.com>
2020-05-06 10:55:28 +0200

    creating repositories

---
+++ src/main/java/com/example/recipe/repositories/UnitofMeasureRepository.java
@@ -0,0 +1,7 @@
+package com.example.recipe.repositories;
+
+import com.example.recipe.domain.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitofMeasureRepository extends CrudRepository<UnitOfMeasure,Long> {
+}
