c94ab52896e6024dc2c390aa0d43a2e624f26e97
Ágocsi-Kiss Bence <akbence007@gmail.com>
2020-07-22 14:44:25 +0200

    simple junit5 test

---
+++ src/test/java/com/example/recipe/domain/CategoryTest.java
@@ -0,0 +1,34 @@
+package com.example.recipe.domain;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryTest {
+
+    Category category;
+
+    @BeforeEach
+    public void setUp() {
+        category = new Category();
+    }
+
+    @Test
+    void getId() {
+        Long idValue = 4L;
+
+        category.setId(idValue);
+
+        assertEquals(4L, category.getId());
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getRecipes() {
+    }
+}
