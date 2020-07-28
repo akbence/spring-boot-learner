4751762f3c48558293cdb8f0725a4845c05f2b3c
Ágocsi-Kiss Bence <akbence007@gmail.com>
2020-07-28 11:50:22 +0200

    create a recipe

--- src/main/java/com/example/recipe/controllers/RecipeController.java
+++ src/main/java/com/example/recipe/controllers/RecipeController.java
@@ -2,11 +2,11 @@ package com.example.recipe.controllers;
 
 
 
+import com.example.recipe.commands.RecipeCommand;
 import com.example.recipe.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RecipeController {
@@ -23,5 +23,19 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe",new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
